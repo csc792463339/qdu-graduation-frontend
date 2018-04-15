7e08ef2783cae45222df3b4a9dd92b1833514592
chensicao <hellocsc@qq.com>
2018-04-15 15:00:24 +0800

    管理员-老师申请页面更新

--- js/jquery-weui.js
+++ js/jquery-weui.js
@@ -6,6 +6,25 @@
 /* global $:true */
 /* global WebKitCSSMatrix:true */
 
+function fmtDate(obj){
+    var date =  new Date(obj);
+    var seperator1 = "-";
+    var seperator2 = ":";
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
+        + " " + date.getHours() + seperator2 + date.getMinutes()
+        + seperator2 + date.getSeconds();
+    return currentdate;
+}
+
+
 (function($) {
   "use strict";
 
